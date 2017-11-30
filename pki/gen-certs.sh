207dd941be671a988ae0302e4aa0019b6a1d46db
Jeff Mendoza <jeffmendoza@google.com>
2017-11-30 15:38:37 -0800

    redo certs

--- pki/gen-certs.sh
+++ pki/gen-certs.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
+set -e
+
 cfssl gencert -initca ca-csr.json | cfssljson -bare ca
 cfssl gencert \
   -ca=ca.pem \
   -ca-key=ca-key.pem \
   -config=ca-config.json \
-  -hostname=127.0.0.1,image-signature-webhook,image-signature-webhook.kube-system,image-signature-webhook.default,image-signature-webhook.default.svc \
+  -hostname=127.0.0.1,container-analysis-webhook,container-analysis-webhook.kube-system,container-analysis-webhook.default,container-analysis-webhook.default.svc \
   -profile=default \
-  image-signature-webhook-csr.json | cfssljson -bare image-signature-webhook
+  container-analysis-webhook-csr.json | cfssljson -bare container-analysis-webhook
 
-kubectl create secret tls tls-image-signature-webhook \
-  --cert=image-signature-webhook.pem \
-  --key=image-signature-webhook-key.pem
+kubectl create secret tls tls-container-analysis-webhook \
+  --cert=container-analysis-webhook.pem \
+  --key=container-analysis-webhook-key.pem
